b17e25e324fe09b8c5d8d5e126ad2a888f562052
Amamiyakun02 <amamiyakun02@gmail.com>
2025-01-09 10:40:49 +0800

    add libtorch

---
+++ include/libclass.cpp
@@ -0,0 +1,8 @@
+//
+// Created by amamiya on 1/8/25.
+//
+
+#include "libclass.h"
+
+namespace lib1 {
+} // lib1
---
+++ include/libclass.h
@@ -0,0 +1,16 @@
+//
+// Created by amamiya on 1/8/25.
+//
+
+#ifndef LIBCLASS_H
+#define LIBCLASS_H
+
+namespace lib1 {
+
+class libclass {
+
+};
+
+} // lib1
+
+#endif //LIBCLASS_H
